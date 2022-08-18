3548cf09942a781adc866d3f3a02772499d56574
Philipp Richter <richterphilipp.pops@gmail.com>
2022-08-18 14:33:50 +0200

    Update dependencies and fix type issues

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import {
   sleep,
   staticClasses,
   ToggleField,
+  AppOverview,
+  DisplayStatus
 } from "decky-frontend-lib";
 import { useEffect, useState, VFC } from "react";
 import { FaPause } from "react-icons/fa";
@@ -25,18 +27,20 @@ interface AppLifetimeObject {
   bRunning: boolean; // if the game is running or not
 }
 
-interface AppObject {
-  appid: number; // base
-  instanceid: number; // an extension to keep track if the pid of the reaper process
+interface AppOverviewExt extends AppOverview {
+  appid: string; // base
   display_name: string; // base
+  display_status: DisplayStatus; // base
+  sort_as: string; // base
   icon_data: string; // base, base64 encoded image
   icon_data_format: string; // base, image type without "image/" (e.g.: jpg, png)
   icon_hash: string; // base, url hash to fetch the icon for steam games (e.g.: "/assets/" + appid + "_icon.jpg?v=" + icon_hash)
   m_gameid: string; // base, id for non-steam games
+  instanceid: number; // an extension to keep track if the pid of the reaper process
   is_paused: boolean; // extension to keep track of a paused application
 }
 
-const Item: VFC<{ serverAPI: ServerAPI, app: AppObject }> = ({serverAPI, app}) => {
+const Item: VFC<{ serverAPI: ServerAPI, app: AppOverviewExt }> = ({serverAPI, app}) => {
   const [isPaused, setIsPaused] = useState<boolean>(app.is_paused);
 
   return (
@@ -45,7 +49,11 @@ const Item: VFC<{ serverAPI: ServerAPI, app: AppObject }> = ({serverAPI, app}) =
       label={app.display_name}
       description={isPaused ? "Paused" : "Running"}
       disabled={!app.instanceid}
-      icon={((app.icon_data && app.icon_data_format) || app.icon_hash) ? <img style={{maxWidth: 32, maxHeight: 32}} src={(app.icon_data) ? ("data:image/" + app.icon_data_format + ";base64," + app.icon_data) : ("/assets/" + app.appid + "_icon.jpg?v=" + app.icon_hash) } /> : null}
+      icon={((app.icon_data && app.icon_data_format) || app.icon_hash) ? 
+        <img style={{maxWidth: 32, maxHeight: 32}} src={(app.icon_data) ?
+          ("data:image/" + app.icon_data_format + ";base64," + app.icon_data) :
+            ("/assets/" + app.appid + "_icon.jpg?v=" + app.icon_hash) } /> :
+          null}
       onChange={async (state) => {
         let ret = await serverAPI.callPluginMethod<{pid: number}, boolean>((state?"pause":"resume"), {pid: app.instanceid});
         if (!ret.success || !ret.result) {
@@ -62,24 +70,24 @@ const Item: VFC<{ serverAPI: ServerAPI, app: AppObject }> = ({serverAPI, app}) =
 };
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
-  const [runningApps, setRunningApps] = useState<AppObject[]>([]);
+  const [runningApps, setRunningApps] = useState<AppOverviewExt[]>([]);
 
   useEffect(() => {
     const refresh = async (app: AppLifetimeObject) => {
       await sleep(100); // wait for a bit before going on
-      const runningAppsTmp: AppObject[] = Router.RunningApps;
+      const runningAppsTmp: AppOverviewExt[] = Router.RunningApps as AppOverviewExt[];
       const toKeep = await Promise.all(runningAppsTmp.map((async (a) => {
         // do not keep an instance that's going to die
         if (!app.bRunning && a.instanceid !== 0 && app.nInstanceID !== 0 && a.instanceid === app.nInstanceID) {
           return false;
         }
-        if (!a.instanceid && app.nInstanceID !== 0 && a.appid !== 0 && app.unAppID !== 0 && a.appid === app.unAppID) {
+        if (!a.instanceid && app.nInstanceID !== 0 && Number(a.appid) !== 0 && app.unAppID !== 0 && Number(a.appid) === app.unAppID) {
           a.instanceid = app.nInstanceID;
         }
-        if (!a.instanceid && a.appid !== 0) {
+        if (!a.instanceid && Number(a.appid) !== 0) {
           // we may need to retry to get the pid since the process is still getting started
           for (let i = 0; i < 3; ++i) {
-            const ret = await serverAPI.callPluginMethod<{appid: number}, number>("pid_from_appid", {appid: a.appid});
+            const ret = await serverAPI.callPluginMethod<{appid: number}, number>("pid_from_appid", {appid: Number(a.appid)});
             if (ret.success && ret.result !== 0) {
               a.instanceid = ret.result;
               break;
@@ -92,7 +100,7 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
           for (let i = 0; i < 3; ++i) {
             const ret = await serverAPI.callPluginMethod<{pid: number}, number>("appid_from_pid", {pid: a.instanceid});
             if (ret.success && ret.result !== 0) {
-              a.appid = ret.result;
+              a.appid = String(ret.result);
               break;
             }
             await sleep(100);
@@ -107,7 +115,7 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
         }
         return true;
       })));
-      const newRunningAppsTmp: AppObject[] = [];
+      const newRunningAppsTmp: AppOverviewExt[] = [];
       for (let i = 0; i < toKeep.length; ++i) {
         if (toKeep[i]) {
           newRunningAppsTmp.push(runningAppsTmp[i]);
